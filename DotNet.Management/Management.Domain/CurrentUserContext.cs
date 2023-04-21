c2f7a58e2681d20ee89989b517858eb0c0ca837c
Jeff-Chi <asnotracking@gmail.com>
2023-04-21 17:54:17 +0800

    add current user context

---
+++ DotNet.Management/Management.Domain/CurrentUserContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Domain
+{
+    /// <summary>
+    /// 当前登录用户
+    /// </summary>
+    public class CurrentUserContext: ICurrentUserContext
+    {
+        public long? Id { get; private set; }
+
+        public string? UserName { get; private set; }
+
+        public HashSet<string> PermissionCodes { get; private set; } = new HashSet<string>();
+
+        public HashSet<long> RoleIds { get; private set; } = new HashSet<long>();
+
+        public void SetValue(
+            long? id, 
+            string? userName, 
+            HashSet<string> permissionCods, 
+            HashSet<long> roleIds)
+        {
+            Id = id;
+            UserName = userName;
+            PermissionCodes = permissionCods;
+            RoleIds = roleIds;
+        }
+    }
+}
---
+++ DotNet.Management/Management.Domain/ICurrentUserContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Domain
+{
+    public interface ICurrentUserContext
+    {
+        long? Id { get; }
+        string? UserName { get; }
+        HashSet<string> PermissionCodes { get;}
+        HashSet<long> RoleIds { get; }
+
+        void SetValue(
+            long? id,
+            string? userName,
+            HashSet<string> permissionCods,
+            HashSet<long> roleIds);
+    }
+}
